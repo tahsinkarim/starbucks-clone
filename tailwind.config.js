686883484d15ab67267aa5e22c87ffd1ed7c5332
tahsinkarim <tahsinnkarim@gmail.com>
2022-06-05 00:43:31 +0600

    Footer Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
         textGreen: "#1e3932",
         greenHover: "rgba(30,57,50,.1)",
         whiteHover: "rgba(255, 255, 255, 0.12)",
+        grayText: "rgba(0,0,0,.58)",
+        grayHover: "rgba(0, 0, 0, 0.87)",
         darkPink: "#F06464",
         lightPink: "#FA91AA",
         
