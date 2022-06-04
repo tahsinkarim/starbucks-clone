6212a88ac329654e9943a8fd29d0400db4413457
tahsinkarim <tahsinnkarim@gmail.com>
2022-06-04 21:14:21 +0600

    updated responsive main

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
       colors: {
         starGreen: "#D4E9E2",
         textGreen: "#1e3932",
+        greenHover: "rgba(30,57,50,.1)",
+        whiteHover: "rgba(255, 255, 255, 0.12)",
         darkPink: "#F06464",
         lightPink: "#FA91AA",
+        
       },
     },
   },
