2b7b6011f6c799227bff919f0c8dcb277ccdf642
tahsinkarim <tahsinnkarim@gmail.com>
2022-06-09 16:44:06 +0600

    image link updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./docs/**/*.{html,js}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'hero-image': "url('/images/1.webp')",
+      },
       fontSize: {
         sm: ['14px'],
       },
